11cb75e79cca45d607dcb0ca7273fdc75a052abd
gevrest <149320260+gevrest@users.noreply.github.com>
2025-07-15 17:12:42 +0500

    Added health

---
+++ Assets/Code/HealthComponent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+
+    public int health;
+    private bool isDead = false;
+
+    public void DealDamage(int damage)
+    {
+        if (!isDead)
+        {
+            health = Mathf.Max(0, health - damage);
+        }
+
+        if (health == 0)
+        {
+            isDead = true;
+            Destroy(gameObject);
+        }
+    }
+
+}
