503f132103d72c067fbe9c9a96522fa1ade2a499
gevrest <149320260+gevrest@users.noreply.github.com>
2025-08-08 21:34:25 +0500

    Added rockets and reload

--- Assets/Code/Weapon/Rocket.cs
+++ Assets/Code/Weapon/Rocket.cs
@@ -2,8 +2,32 @@ using UnityEngine;
 
 namespace Game
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class Rocket : MonoBehaviour
     {
+        private Rigidbody _rigidbody;
 
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        public bool IsActive { get; set; }
+
+        public void Sleep()
+        {
+            _rigidbody.Sleep();
+            gameObject.SetActive(false);
+            IsActive = false;
+        }
+
+        public void Run(Vector3 path, Vector3 startPosition)
+        {
+            transform.position = startPosition;
+            gameObject.SetActive(true);
+            _rigidbody.WakeUp();
+            _rigidbody.AddForce(path);
+            IsActive=true;
+        }
     }
 }
--- Assets/Code/Weapon/Weapon.cs
+++ Assets/Code/Weapon/Weapon.cs
@@ -4,14 +4,103 @@ namespace Game
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] private Transform _barrel;
+        [SerializeField] private Rocket _rocket;
+        [SerializeField] private int _countInClip;
+        [SerializeField] private float _force;
+        [SerializeField] private float _shotDelay;
+
+        private Transform _rocketRoot;
+        private Rocket[] _rockets;
+
+        private void Start()
+        {
+            _rocketRoot = new GameObject("Rocket Root").transform;
+            Reload();
+        }
+
         public void Fire()
         {
+            if (TryGetActiveRocket(out Rocket rocket))
+            {
+                rocket.Run(_barrel.forward *  _force, _barrel.position);
+            }
+        }
 
+        private bool TryGetActiveRocket(out Rocket activeRocket)
+        {
+            int candidate = -1;
+            activeRocket = default;
+
+            if (_rockets == null)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < _countInClip; i++)
+            {
+                Rocket rocket = _rockets[i];
+
+                if (rocket == null)
+                {
+                    continue;
+                }
+
+                if (rocket.IsActive)
+                {
+                    return true;
+                }
+
+                candidate = i;
+                break;
+            }
+
+            if (candidate == -1)
+            {
+                return false;
+            }
+
+            activeRocket = _rockets[candidate];
+            return true;
         }
 
         public void Reload()
         {
+            if (IsAnyActiveRocket())
+            {
+                return;
+            }
+            _rockets = new Rocket[_countInClip];
+            for (int i = 0; i < _countInClip; i++)
+            {
+                Rocket rocket = Instantiate(_rocket, _rocketRoot);
+                rocket.Sleep();
+                _rockets[i] = rocket;
+            }
+        }
 
+        private bool IsAnyActiveRocket()
+        {
+            if (_rockets == null)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < _countInClip; i++)
+            {
+                Rocket rocket = _rockets[i];
+
+                if (rocket == null)
+                {
+                    continue;
+                }
+
+                if (rocket.IsActive)
+                {
+                       return true;
+                }
+            }
+            return false;
         }
     }
 }
