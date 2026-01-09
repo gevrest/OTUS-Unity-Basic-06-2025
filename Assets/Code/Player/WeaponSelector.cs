e1318995045b7290e7e4e1702d5e5ccf92745ce4
gevrest <149320260+gevrest@users.noreply.github.com>
2026-01-09 17:29:15 +0500

    Rebase fixes

--- Assets/Code/Player/WeaponSelector.cs
+++ Assets/Code/Player/WeaponSelector.cs
@@ -1,27 +1,67 @@
+using UnityEngine;
+
 namespace Game
 {
-    public class WeaponSelector
+    public sealed class WeaponSelector
     {
-        private int _weaponIndex;
+        private readonly Weapon[] _weapons;
+        private int _currentIndex = 0;
+        private Weapon _currentWeapon;
+
+        public WeaponSelector(Weapon[] weapons)
+        {
+            _weapons = weapons;
+            for (int i = 0; i < _weapons.Length; i++)
+            {
+                Weapon weapon = _weapons[i];
+                weapon.SetActive(false);
+            }
+        }
 
         public void NextWeapon()
         {
+            _currentIndex++;
             SelectWeapon();
         }
 
         public void PreviosWeapon()
         {
+            _currentIndex--;
             SelectWeapon();
         }
 
-        private void SelectWeapon()
+        public void SelectWeapon()
         {
+            if (_currentWeapon != null)
+            {
+                _currentWeapon.SetActive(false);
+            }
 
+            int index = Mathf.Abs(_currentIndex % _weapons.Length);
+
+            _currentWeapon = _weapons[index];
+            _currentWeapon.SetActive(true);
+        }
+
+        public void Fire()
+        {
+            _currentWeapon.Fire();
+        }
+
+        public void Reload()
+        {
+            if (_currentWeapon is IReloadable reload)
+            {
+                reload.Reload();
+            }
         }
 
         public void ReleaseTrigger()
         {
-            _currentWeapon.ReleaseTrigger();
+            if (_currentWeapon is IReleasable release)
+            {
+                release.ReleaseTrigger();
+            }
         }
     }
 }
